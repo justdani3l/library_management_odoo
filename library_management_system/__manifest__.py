9b60ffea7ee49c4a2815ec2a0e3a7f74169ad294
daniel <danielmullaj8@gmail.com>
2024-08-09 07:36:32 +0200

    update manifest & member_views

--- library_management_system/__manifest__.py
+++ library_management_system/__manifest__.py
@@ -1,16 +1,17 @@
 {
-    'name': 'Daniel',
+    'name': 'Library Management',
     'version': '1.0',
     'summary': 'Summery',
     'description': 'Library Management',
     'category': 'Library',
     'author': 'Author',
     'website': 'Website',
-    'depends': ['Depends'],
+    'depends': ['base'],
     'data': [
-        'views/member_views_xml',
+        'views/member_views.xml',
     ],
 
     'installable': True,
-    'auto_install': False
+    'auto_install': False,
+    'application': True,
 }
