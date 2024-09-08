6cd8dc55929fa15a3ad95e073dba3e7d75395865
daniel <danielmullaj8@gmail.com>
2024-09-08 20:31:59 +0200

    Remove om_pharmacy from the repository

--- om_pharmacy/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import pharmacy_medicine
-from . import pharmacy_medicine_category
-from . import pharmacy_prescription
-from . import pharmacy_stock_movement
--- om_pharmacy/models/pharmacy_medicine_category.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class PharmacyMedicineCategory(models.Model):
-    _name = 'pharmacy.medicine.category'
-    _description = 'Pharmacy Medicine Category'
-
-    name = fields.Char(string='Category Name', required=True)
-    description = fields.Text(string='Description')
